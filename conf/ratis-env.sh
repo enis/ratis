e64a580ed867a8ed14c6f050adef98937adc4064
Enis Soztutar <enis@apache.org>
2017-03-28 19:39:27 -0700

    RATIS-29 v2

--- conf/ratis-env.sh
+++ conf/ratis-env.sh
@@ -33,15 +33,11 @@
 # The maximum amount of heap to use. Default is left to JVM default.
 # export RATIS_HEAPSIZE=1G
 
-# Uncomment below if you intend to use off heap cache. For example, to allocate 8G of 
-# offheap, set the value to "8G".
-# export RATIS_OFFHEAPSIZE=1G
-
 # Extra Java runtime options.
 # Below are what we set by default.  May only work with SUN JVM.
 # For more on why as well as other possible settings,
 # see http://ratis.apache.org/book.html#performance
-export RATIS_OPTS="-XX:+UseConcMarkSweepGC"
+export RATIS_OPTS="-XX:+UseG1GC"
 
 # Uncomment one of the below three options to enable java garbage collection logging for the server-side processes.
 
@@ -52,7 +48,7 @@ export RATIS_OPTS="-XX:+UseConcMarkSweepGC"
 # If FILE-PATH is not replaced, the log file(.gc) would still be generated in the RATIS_LOG_DIR .
 # export SERVER_GC_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:<FILE-PATH>"
 
-# This enables basic GC logging to its own file with automatic log rolling. Only applies to jdk 1.6.0_34+ and 1.7.0_2+.
+# This enables basic GC logging to its own file with automatic log rolling.
 # If FILE-PATH is not replaced, the log file(.gc) would still be generated in the RATIS_LOG_DIR .
 # export SERVER_GC_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:<FILE-PATH> -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=1 -XX:GCLogFileSize=512M"
 
@@ -65,28 +61,16 @@ export RATIS_OPTS="-XX:+UseConcMarkSweepGC"
 # If FILE-PATH is not replaced, the log file(.gc) would still be generated in the RATIS_LOG_DIR .
 # export CLIENT_GC_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:<FILE-PATH>"
 
-# This enables basic GC logging to its own file with automatic log rolling. Only applies to jdk 1.6.0_34+ and 1.7.0_2+.
+# This enables basic GC logging to its own file with automatic log rolling.
 # If FILE-PATH is not replaced, the log file(.gc) would still be generated in the RATIS_LOG_DIR .
 # export CLIENT_GC_OPTS="-verbose:gc -XX:+PrintGCDetails -XX:+PrintGCDateStamps -Xloggc:<FILE-PATH> -XX:+UseGCLogFileRotation -XX:NumberOfGCLogFiles=1 -XX:GCLogFileSize=512M"
-
-# See the package documentation for org.apache.hadoop.ratis.io.hfile for other configurations
-# needed setting up off-heap block caching.
 
 # Uncomment and adjust to enable JMX exporting
 # See jmxremote.password and jmxremote.access in $JRE_HOME/lib/management to configure remote password access.
 # More details at: http://java.sun.com/javase/6/docs/technotes/guides/management/agent.html
-# NOTE: Ratis provides an alternative JMX implementation to fix the random ports issue, please see JMX
-# section in Ratis Reference Guide for instructions.
 
 # export RATIS_JMX_BASE="-Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
 # export RATIS_RMAPSERVER_OPTS="$RATIS_RMAPSERVER_OPTS $RATIS_JMX_BASE -Dcom.sun.management.jmxremote.port=10102"
-
-# File naming hosts on which HRegionServers will run.  $RATIS_HOME/conf/regionservers by default.
-# export RATIS_RMAPSERVERS=${RATIS_HOME}/conf/regionservers
-
-# Uncomment and adjust to keep all the Region Server pages mapped to be memory resident
-#RATIS_RMAPSERVER_MLOCK=true
-#RATIS_RMAPSERVER_UID="ratis"
 
 # Extra ssh options.  Empty by default.
 # export RATIS_SSH_OPTS="-o ConnectTimeout=1 -o SendEnv=RATIS_CONF_DIR"
@@ -94,22 +78,11 @@ export RATIS_OPTS="-XX:+UseConcMarkSweepGC"
 # Where log files are stored.  $RATIS_HOME/logs by default.
 # export RATIS_LOG_DIR=${RATIS_HOME}/logs
 
-# Enable remote JDWP debugging of major Ratis processes. Meant for Core Developers 
+# Enable remote JDWP debugging of major Ratis processes. Meant for Core Developers
 # export RATIS_RMAPSERVER_OPTS="$RATIS_RMAPSERVER_OPTS -Xdebug -Xrunjdwp:transport=dt_socket,server=y,suspend=n,address=8071"
 
 # A string representing this instance of ratis. $USER by default.
 # export RATIS_IDENT_STRING=$USER
-
-# The scheduling priority for daemon processes.  See 'man nice'.
-# export RATIS_NICENESS=10
-
-# The directory where pid files are stored. /tmp by default.
-# export RATIS_PID_DIR=/var/hadoop/pids
-
-# Seconds to sleep between slave commands.  Unset by default.  This
-# can be useful in large clusters, where, e.g., slave rsyncs can
-# otherwise arrive faster than the master can service them.
-# export RATIS_SLAVE_SLEEP=0.1
 
 # The default log rolling policy is RFA, where the log file is rolled as per the size defined for the 
 # RFA appender. Please refer to the log4j.properties file to see more details on this appender.
