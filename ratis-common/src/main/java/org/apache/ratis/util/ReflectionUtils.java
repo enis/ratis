7bbd7ab769ec80b675ef46b4e0d7adae9a2f211e
Enis Soztutar <enis@apache.org>
2017-03-31 17:42:52 -0700

    v3

--- ratis-common/src/main/java/org/apache/ratis/util/ReflectionUtils.java
+++ ratis-common/src/main/java/org/apache/ratis/util/ReflectionUtils.java
@@ -29,7 +29,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Supplier;
 
 import org.apache.ratis.conf.RaftProperties;
 
@@ -56,13 +55,6 @@ public class ReflectionUtils {
    */
   public static <T> T newInstance(Class<T> clazz) {
     return newInstance(clazz, EMPTY_CLASSES);
-  }
-
-  /**
-   * Create an object for the given class using its default constructor.
-   */
-  public static <T> T newInstance(String className) {
-    return newInstance(className, EMPTY_CLASSES);
   }
 
   /**
@@ -95,22 +87,6 @@ public class ReflectionUtils {
     }
   }
 
-  @SuppressWarnings("unchecked")
-  public static <T> T newInstance(String className, Class<? >[] argClasses,
-                                        Object... args) {
-    try {
-      Class<? extends T> resultType = (Class<? extends T>) Class.forName(className);
-      return newInstance(resultType, argClasses, args);
-    } catch (ClassNotFoundException e) {
-      throw new UnsupportedOperationException(
-          "Unable to find " + className, e);
-    }
-  }
-
-  public static <T> T newInstance(Class<T> type, Object... params) {
-    return newInstance(type, findConstructor(type, params), params);
-  }
-
   private static <T> T instantiate(final String className, Constructor<T> ctor, Object[] ctorArgs) {
     try {
       ctor.setAccessible(true);
@@ -127,36 +103,6 @@ public class ReflectionUtils {
     }
   }
 
-  @SuppressWarnings("unchecked")
-  public static <T> Constructor<T> findConstructor(Class<T> type, Object... paramTypes) {
-    Constructor<T>[] constructors = (Constructor<T>[]) type.getDeclaredConstructors();
-    for (Constructor<T> ctor : constructors) {
-      Class<?>[] ctorParamTypes = ctor.getParameterTypes();
-      if (ctorParamTypes.length != paramTypes.length) {
-        continue;
-      }
-
-      boolean match = true;
-      for (int i = 0; i < ctorParamTypes.length && match; ++i) {
-        Class<?> paramType = paramTypes[i].getClass();
-        match = (!ctorParamTypes[i].isPrimitive()) ? ctorParamTypes[i].isAssignableFrom(paramType) :
-            ((int.class.equals(ctorParamTypes[i]) && Integer.class.equals(paramType)) ||
-                (long.class.equals(ctorParamTypes[i]) && Long.class.equals(paramType)) ||
-                (double.class.equals(ctorParamTypes[i]) && Double.class.equals(paramType)) ||
-                (char.class.equals(ctorParamTypes[i]) && Character.class.equals(paramType)) ||
-                (short.class.equals(ctorParamTypes[i]) && Short.class.equals(paramType)) ||
-                (boolean.class.equals(ctorParamTypes[i]) && Boolean.class.equals(paramType)) ||
-                (byte.class.equals(ctorParamTypes[i]) && Byte.class.equals(paramType)));
-      }
-
-      if (match) {
-        return ctor;
-      }
-    }
-    throw new UnsupportedOperationException(
-        "Unable to find suitable constructor for class " + type.getName());
-  }
-
   /** Is the given object an instance of one of the given classes? */
   public static boolean isInstance(Object obj, Class<?>... classes) {
     for(Class<?> c : classes) {
@@ -165,36 +111,6 @@ public class ReflectionUtils {
       }
     }
     return false;
-  }
-
-  /**
-   * Create a memoized supplier which gets a value by invoking the initializer once
-   * and then keeps returning the same value as its supplied results.
-   *
-   * @param initializer to supply at most one non-null value.
-   * @param <T> The supplier result type.
-   * @return a memoized supplier which is thread-safe.
-   */
-  public static <T> Supplier<T> memoize(Supplier<T> initializer) {
-    Objects.requireNonNull(initializer, "initializer == null");
-    return new Supplier<T>() {
-      private volatile T value = null;
-
-      @Override
-      public T get() {
-        T v = value;
-        if (v == null) {
-          synchronized (this) {
-            v = value;
-            if (v == null) {
-              v = value = Objects.requireNonNull(initializer.get(),
-                  "initializer.get() returns null");
-            }
-          }
-        }
-        return v;
-      }
-    };
   }
 
   public static <BASE> Class<? extends BASE> getClass(
